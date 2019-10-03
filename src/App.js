fe853b134d1567af513368a595996128d049fd7e
Oventura19 <47031203+Oventura19@users.noreply.github.com>
2019-10-02 20:27:02 -0500

    primera-aplicacion-react
    Tu primera aplicación de React

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import './App.css';
 
 function App() {
   return ( <div className = "App">
-    <h1>"Mi primera aplicación"</h1>
-    <p>"Esta es mi primera aplicación en React, está padrísimo!"</p>
+    <h1>Mi primera aplicación</h1>
+    <p>Esta es mi primera aplicación en React, está padrísimo!</p>
   </div>
   );
 }
 
 export default App;
+
